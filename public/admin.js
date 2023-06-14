3d161f615d7c0715fa922edcc094181f78da638b
Britta Sanchez <131740378+Bee1993@users.noreply.github.com>
2023-06-14 01:46:27 -0700

    edited admin

--- public/admin.js
+++ public/admin.js
@@ -1,6 +1,21 @@
+const { json } = require("body-parser");
 
 // Your Code Here
-let response = await fetch('http://localhost:3001/listBooks'){
+let response = await fetch('http://localhost:3001/listBooks'{
     method: "GET"
-}
+});
+let books = await response.json();
+console.log(books);
 
+
+let response = await fetch('http://localhost:3001/updateBook',{
+method: "PATCH",
+headers:{
+    'Content-Type': 'application/json'}
+    body: json.stringify({
+       "id" : 3
+       "title" : 
+    })
+});
+let updatedBook = await response.json();
+console.log(updatedBook)
