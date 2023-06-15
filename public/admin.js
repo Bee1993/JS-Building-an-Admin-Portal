0fe1e0835469796fce631a42650313f1ccd8e674
Britta Sanchez <131740378+Bee1993@users.noreply.github.com>
2023-06-14 20:53:20 -0700

    admin edited

--- public/admin.js
+++ public/admin.js
@@ -1,21 +1,43 @@
 const { json } = require("body-parser");
 
 // Your Code Here
-let response = await fetch('http://localhost:3001/listBooks'{
-    method: "GET"
-});
-let books = await response.json();
-console.log(books);
+async function main() {
 
+    let response = await fetch('http://localhost:3001/listBooks')
 
-let response = await fetch('http://localhost:3001/updateBook',{
-method: "PATCH",
-headers:{
-    'Content-Type': 'application/json'}
-    body: json.stringify({
-       "id" : 3
-       "title" : 
+    let books = await response.json()
+
+    books.forEach(renderBook)
+}
+
+function renderBook(book) {
+    let root = document.querySelector('#root')
+
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+    saveButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+            })
+        })
     })
-});
-let updatedBook = await response.json();
-console.log(updatedBook)
+
+    li.append(quantityInput, saveButton)
+
+    root.append(li)
+}
+
+main();
